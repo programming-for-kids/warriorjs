e0a295a8d4bfeaedd9df8342f523d26c94159b50
think-in-universe <46699230+think-in-universe@users.noreply.github.com>
2022-02-19 20:50:24 +0800

    chore: fix build script

--- packages/warriorjs-cli/bin/play.sh
+++ packages/warriorjs-cli/bin/play.sh
@@ -1,6 +1,6 @@
-cd ../warriorjs-units
-npm install && npm run build
 cd ../warriorjs-abilities
+npm install && npm run build
+cd ../warriorjs-units
 npm install && npm run build
 cd ../warriorjs-tower-beginner
 npm install && npm run build
